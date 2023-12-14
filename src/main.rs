664281727dd4733c42434522751f5919dd68b607
PawsOfDestruction <purvanjaro2@gmail.com>
2023-12-14 23:34:18 +0200

    it works and extracts as should be

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::{path::Path,process::Command, fs::File};
+use std::{path::Path,process::Command, fs::{File, self}};
 
 fn main() {
 
     let tf2_dir = "E:/SteamLibrary/steamapps/common/Team Fortress 2".to_owned();
     let sfm_dir = "E:/SteamLibrary/steamapps/common/SourceFilmmaker/game/tf";
     // TODO: remove and write directly to SFM folder
+    let destination = "E:/test";
 
     let mut vpk_exe_path = tf2_dir.clone().to_owned();
 
@@ -61,23 +62,62 @@ fn extract_vpk_file(vpk_file_path: &str,tf2_game_path: &str, destination: &str) 
     println!("tf2_dir: {tf2_dir}");
 
     let mut vpk_cmd = Command::new(tf2_dir);
-    vpk_cmd.arg(vpk_file_path);
+    vpk_cmd.arg(vpk_file_path).output().unwrap();
 
-    let output = match vpk_cmd.output() {
-        Ok(data) => data,
-        Err(err) => {
-            if err.kind().to_string() == "PermissionDenied" {
-                panic!("Please run this file with admin permissions!");
+
+    let mut vpk_output_dir = tf2_game_path.clone().to_owned();
+
+    let split_name = vpk_file_name.split(".").collect::<Vec<&str>>();
+    
+    let output_dir_name = split_name.first().unwrap();
+    vpk_output_dir.push_str("/tf/");
+    vpk_output_dir.push_str(output_dir_name);
+    
+    println!("output dir: {vpk_output_dir}");
+    
+    let did_vpk_extract = Path::new(&vpk_output_dir.to_owned().to_string()).exists();
+
+    if (!did_vpk_extract) {
+        panic!("Something went wrong while extracting the VPK file.");
+    }
+
+    
+    // remove unneeded folders (only maps, models, materials, particles, sound)
+    let needed_folders = vec!["maps", "models", "materials", "particles", "sound"];
+    
+    if let Ok(entries) = fs::read_dir(vpk_output_dir.to_string()) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_dir() {
+                    
+                    // If you want to work with the directory path
+                    println!("Folder found: {:?}", path);
+
+                    // If you want to extract the directory name as a string
+                    if let Some(folder_name) = path.file_name() {
+                        if let Some(name) = folder_name.to_str() {
+                            println!("Folder name: {}", name);
+                            if needed_folders.contains(&name) {
+
+                                // use 'rename' function to move the newly extracted folder to the destination.
+                                let mut destination_folder = destination.to_owned();
+                                destination_folder.push_str("/");
+                                destination_folder.push_str(name);
+                                println!("moving from {} to {destination_folder}",path.canonicalize().unwrap().display());
+                                fs::rename(path, destination_folder).unwrap();
+                            }
+                        }
+                    }
+                }
             }
-            return ();
-        },
-    }.stdout;
+        }
+    } else {
+        println!("Failed to read directory");
+    }
 
-    
-    let outputString = String::from_utf8(output).unwrap().to_string();
-    
-    let check = Path::new("{tf2_game_path}").exists();
+    // delete remaining folders since we don't need them 
+    fs::remove_dir_all(vpk_output_dir).unwrap();
 
-    println!("Output: {}",outputString);
 }
 
