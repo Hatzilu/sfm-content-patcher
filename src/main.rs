24c3708811dd36bcbc0c66cad3f350382a6bee80
PawsOfDestruction <purvanjaro2@gmail.com>
2023-12-14 21:34:14 +0200

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::{process::Command};
+use std::process::Command;
 
 
 fn main() {
 
     let tf2_dir = "E:/SteamLibrary/steamapps/common/Team Fortress 2".to_owned();
     let sfm_dir = "E:/SteamLibrary/steamapps/common/SourceFilmmaker/game/tf";
+    // TODO: remove and write directly to SFM folder
 
     // TODO: auto-detect the tf2 and sfm directories. or ask user for input if auto-detection fails
     println!("Your TF2 directory sits at {}", tf2_dir);
@@ -12,7 +13,8 @@ fn main() {
 
     let mut vpk_path = tf2_dir.clone().to_owned();
     vpk_path.push_str("/tf/tf2_misc_dir.vpk");
-    extract_vpk_file(&vpk_path,&tf2_dir );
+
+    extract_vpk_file(&vpk_path,&tf2_dir,destination);
 
     println!("Hello, world!");
 }
@@ -29,27 +31,31 @@ fn get_file_names(path: &str, tf2_game_path: &str) -> Vec<String> {
 
     tf2_dir.push_str("/bin/vpk.exe");
 
-    println!("dir: {}", tf2_dir);
-    println!("path: {}", path);
-
     let mut vpk_cmd = Command::new(tf2_dir);
-    vpk_cmd.arg("L").arg(path);
-
-    let d = format!("{:?}",vpk_cmd);
-
-    println!("{:?}",d);
+    vpk_cmd.arg("l").arg(path);
 
     let output = String::from_utf8(vpk_cmd.output().unwrap().stdout).unwrap().to_string();
 
-    for file_name in output.lines() {
-        if file_name.starts_with("maps") || file_name.starts_with("models") || file_name.starts_with("materials") || file_name.starts_with("particles")  || file_name.starts_with("sound") {
-            file_names.push(file_name.to_string());
+    for raw_file_name in output.lines() {
+        if raw_file_name.starts_with("maps") || raw_file_name.starts_with("models") || raw_file_name.starts_with("materials") || raw_file_name.starts_with("particles")  || raw_file_name.starts_with("sound") {
+            file_names.push(raw_file_name.to_string());
         }
     }
 
     return file_names;
 }
 
-fn extract_vpk_file(vpk_file_path: &str, destination: &str) {
+fn extract_vpk_file(vpk_file_path: &str,tf2_game_path: &str, destination: &str) {
+    let file_names = get_file_names(&vpk_file_path, &tf2_game_path);
+    println!("{}",file_names.last().unwrap().to_string());
 
+    let tf2_dir = tf2_game_path.clone().to_owned();
+
+    // let mut vpk_cmd = Command::new(tf2_dir);
+    // vpk_cmd.arg("x").arg(vpk_file_path);
+
+    // let output = String::from_utf8(vpk_cmd.output().unwrap().stdout).unwrap().to_string();
+
+    // println!("Output: {}",output);
 }
+
